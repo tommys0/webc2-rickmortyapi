0dd12e00b832e5f0d010ba1f004d0935b8c3f098
tommys0 <149568540+tommys0@users.noreply.github.com>
2024-03-19 12:37:52 +0100

    Get Data from API

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 const api = 'https://rickandmortyapi.com/api/character'
+
+async function getData() {
+    const res = await fetch(api)
+
+    if (!res.ok) {
+        throw Error ("Failed to fetch data.")
+    }
+
+    return res.json;
+}
