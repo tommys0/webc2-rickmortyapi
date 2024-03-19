ef178e4a742141dd842582bfd37f6c834e01ecd0
tommys0 <149568540+tommys0@users.noreply.github.com>
2024-03-19 12:59:01 +0100

    Display people

--- script.js
+++ script.js
@@ -4,8 +4,38 @@ async function getData() {
     const res = await fetch(api)
 
     if (!res.ok) {
-        throw Error ("Failed to fetch data.")
+        throw Error("Failed to fetch data.")
     }
 
-    return res.json;
+    return res.json()
 }
+
+async function displayData() {
+    const data = await getData()
+    console.log(data)
+
+    data.results.forEach(result => {
+        const characterInfo = document.createElement('div')
+
+        const name = document.createElement('p')
+        name.textContent = 'Name: ' + result.name
+
+        const status = document.createElement('p')
+        status.textContent = 'Status: ' + result.status
+
+        const type = document.createElement('p')
+        type.textContent = 'Type: ' + result.type
+
+        const image = document.createElement('img')
+        image.src = result.image
+
+        characterInfo.appendChild(name);
+        characterInfo.appendChild(status);
+        characterInfo.appendChild(type);
+        characterInfo.appendChild(image)
+
+        document.body.appendChild(characterInfo);
+    });
+}
+
+displayData();
