cac870bdb47393c6744c26e5151c3508cb41f59f
tommys0 <149568540+tommys0@users.noreply.github.com>
2024-03-19 13:20:57 +0100

    Added Next&Prev Button + classes

--- script.js
+++ script.js
@@ -1,41 +1,71 @@
-const api = 'https://rickandmortyapi.com/api/character'
+let pageNumber = 1;
 
 async function getData() {
-    const res = await fetch(api)
+    const api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+    const res = await fetch(api);
 
     if (!res.ok) {
-        throw Error("Failed to fetch data.")
+        throw Error("Failed to fetch data.");
     }
 
-    return res.json()
+    return res.json();
+}
+
+function nextPage() {
+    pageNumber++;
+    displayData();
+}
+
+function previousPage() {
+    if (pageNumber > 1) {
+        pageNumber--;
+        displayData();
+    }
 }
 
 async function displayData() {
-    const data = await getData()
-    console.log(data)
+    const data = await getData();
+    console.log(data);
+
+    document.body.innerHTML = '';
+
+    const buttonNext = document.createElement('button');
+    buttonNext.addEventListener('click', nextPage);
+    buttonNext.textContent = 'Next';
+
+    const buttonPrevious = document.createElement('button');
+    buttonPrevious.addEventListener('click', previousPage);
+    buttonPrevious.textContent = 'Previous';
 
     data.results.forEach(result => {
-        const characterInfo = document.createElement('div')
+        const characterInfo = document.createElement('div');
 
-        const name = document.createElement('p')
-        name.textContent = 'Name: ' + result.name
+        const name = document.createElement('p');
+        name.textContent = 'Name: ' + result.name;
+        name.className = 'nameText';
 
-        const status = document.createElement('p')
-        status.textContent = 'Status: ' + result.status
+        const status = document.createElement('p');
+        status.textContent = 'Status: ' + result.status;
+        status.className = 'statusText';
 
-        const type = document.createElement('p')
-        type.textContent = 'Type: ' + result.type
+        const type = document.createElement('p');
+        type.textContent = 'Type: ' + result.type;
+        type.className = 'typeText';
 
-        const image = document.createElement('img')
-        image.src = result.image
+        const image = document.createElement('img');
+        image.src = result.image;
+        image.className = 'profileImage';
 
         characterInfo.appendChild(name);
         characterInfo.appendChild(status);
         characterInfo.appendChild(type);
-        characterInfo.appendChild(image)
+        characterInfo.appendChild(image);
 
         document.body.appendChild(characterInfo);
     });
+
+    document.body.appendChild(buttonPrevious);
+    document.body.appendChild(buttonNext);
 }
 
 displayData();
